6dbaee249de56be3fcf31848950889d1a4a4067b
Mateo <mateochando@gmail.com>
2017-04-14 16:45:01 -0300

    asdasda

---
+++ Trabajo Integral PAV1/Trabajo Integral PAV1/Menu.vb
@@ -0,0 +1,18 @@
+﻿Public Class Menu
+
+    Dim nuevo As Form
+    Private Sub cmd_gestor_Click(sender As Button, e As EventArgs) Handles cmd_gestor_clientes.Click, cmd_gestor_empresas.Click, cmd_gestor_marcas.Click
+
+        Dim ventana As String = sender.Text
+        If ventana = "Gestor empresas" Then
+            nuevo = New gestor_empresas
+        ElseIf ventana = "Gestor marcas" Then
+            nuevo = New gestor_marcas_modelos
+        Else
+            nuevo = New gestor_clientes
+
+        End If
+
+        nuevo.Show()
+    End Sub
+End Class
