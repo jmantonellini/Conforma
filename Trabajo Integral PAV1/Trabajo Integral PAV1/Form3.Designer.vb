0028b546d155ec88f9b65c4fcdaffee2526edce4
juaanma12 <juanmantonellini@gmail.com>
2017-04-14 13:19:18 -0300

    creacion abm empresas

---
+++ Trabajo Integral PAV1/Trabajo Integral PAV1/Form3.Designer.vb
@@ -0,0 +1,247 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form3
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lbl_cuit = New System.Windows.Forms.Label()
+        Me.lbl_nombre = New System.Windows.Forms.Label()
+        Me.lbl_razon_social = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.cmd_nuevo = New System.Windows.Forms.Button()
+        Me.cmd_guardar = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.cmd_salir = New System.Windows.Forms.Button()
+        Me.lbl_datos_empresa = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lbl_cuit
+        '
+        Me.lbl_cuit.AutoSize = True
+        Me.lbl_cuit.Location = New System.Drawing.Point(94, 70)
+        Me.lbl_cuit.Name = "lbl_cuit"
+        Me.lbl_cuit.Size = New System.Drawing.Size(28, 13)
+        Me.lbl_cuit.TabIndex = 0
+        Me.lbl_cuit.Text = "Cuit:"
+        '
+        'lbl_nombre
+        '
+        Me.lbl_nombre.AutoSize = True
+        Me.lbl_nombre.Location = New System.Drawing.Point(75, 35)
+        Me.lbl_nombre.Name = "lbl_nombre"
+        Me.lbl_nombre.Size = New System.Drawing.Size(47, 13)
+        Me.lbl_nombre.TabIndex = 1
+        Me.lbl_nombre.Text = "Nombre:"
+        '
+        'lbl_razon_social
+        '
+        Me.lbl_razon_social.AutoSize = True
+        Me.lbl_razon_social.Location = New System.Drawing.Point(49, 106)
+        Me.lbl_razon_social.Name = "lbl_razon_social"
+        Me.lbl_razon_social.Size = New System.Drawing.Size(73, 13)
+        Me.lbl_razon_social.TabIndex = 0
+        Me.lbl_razon_social.Text = "Razón Social:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(145, 32)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(232, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(145, 67)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(232, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(145, 103)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(232, 20)
+        Me.TextBox3.TabIndex = 2
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(33, 175)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(429, 150)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'cmd_nuevo
+        '
+        Me.cmd_nuevo.Location = New System.Drawing.Point(33, 350)
+        Me.cmd_nuevo.Name = "cmd_nuevo"
+        Me.cmd_nuevo.Size = New System.Drawing.Size(55, 55)
+        Me.cmd_nuevo.TabIndex = 4
+        Me.cmd_nuevo.Text = "Nuevo"
+        Me.cmd_nuevo.UseVisualStyleBackColor = True
+        '
+        'cmd_guardar
+        '
+        Me.cmd_guardar.Location = New System.Drawing.Point(109, 350)
+        Me.cmd_guardar.Name = "cmd_guardar"
+        Me.cmd_guardar.Size = New System.Drawing.Size(55, 55)
+        Me.cmd_guardar.TabIndex = 4
+        Me.cmd_guardar.Text = "Guardar"
+        Me.cmd_guardar.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(367, 147)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Datos obligatorios"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(383, 28)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(17, 24)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "*"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(383, 62)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(17, 24)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "*"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(383, 98)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(17, 24)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "*"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(344, 139)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(17, 24)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "*"
+        '
+        'cmd_salir
+        '
+        Me.cmd_salir.Location = New System.Drawing.Point(407, 350)
+        Me.cmd_salir.Name = "cmd_salir"
+        Me.cmd_salir.Size = New System.Drawing.Size(55, 55)
+        Me.cmd_salir.TabIndex = 4
+        Me.cmd_salir.Text = "Salir"
+        Me.cmd_salir.UseVisualStyleBackColor = True
+        '
+        'lbl_datos_empresa
+        '
+        Me.lbl_datos_empresa.AutoSize = True
+        Me.lbl_datos_empresa.Location = New System.Drawing.Point(11, 9)
+        Me.lbl_datos_empresa.Name = "lbl_datos_empresa"
+        Me.lbl_datos_empresa.Size = New System.Drawing.Size(108, 13)
+        Me.lbl_datos_empresa.TabIndex = 7
+        Me.lbl_datos_empresa.Text = "Datos de la Empresa:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(80, 139)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 13)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Teléfono"
+        '
+        'Form3
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(495, 417)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.lbl_datos_empresa)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmd_salir)
+        Me.Controls.Add(Me.cmd_guardar)
+        Me.Controls.Add(Me.cmd_nuevo)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.lbl_nombre)
+        Me.Controls.Add(Me.lbl_razon_social)
+        Me.Controls.Add(Me.lbl_cuit)
+        Me.Name = "Form3"
+        Me.Text = "Gestor Empresa"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lbl_cuit As System.Windows.Forms.Label
+    Friend WithEvents lbl_nombre As System.Windows.Forms.Label
+    Friend WithEvents lbl_razon_social As System.Windows.Forms.Label
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lbl_razon_social.Click, lbl_cuit.Click
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents cmd_nuevo As System.Windows.Forms.Button
+    Friend WithEvents cmd_guardar As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents cmd_salir As System.Windows.Forms.Button
+    Friend WithEvents lbl_datos_empresa As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+
+
+End Class
