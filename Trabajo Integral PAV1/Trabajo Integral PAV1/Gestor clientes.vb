8e54a0701dfb340204f8626b0a1317fb1a1e2623
Mateo <mateochando@gmail.com>
2017-04-14 16:16:01 -0300

    gestorclientes con boton nuevo

--- Trabajo Integral PAV1/Trabajo Integral PAV1/Gestor clientes.vb
+++ Trabajo Integral PAV1/Trabajo Integral PAV1/Gestor clientes.vb
@@ -5,4 +5,26 @@
     End Sub
 
    
+    Private Sub blanquear_campos() Handles cmd_nuevo.Click, cmd_eliminar.Click
+        cmb_tipo_documento.SelectedIndex = -1
+        cmb_barrio.SelectedIndex = -1
+        cmb_ciudad.SelectedIndex = -1
+        cmb_pais.SelectedIndex = -1
+        cmb_provincia.SelectedIndex = -1
+        cmb_tipo_documento.SelectedIndex = -1
+        txt_altura_calle.Text = ""
+        txt_apellido.Text = ""
+        txt_calle.Text = ""
+        txt_celular.Text = ""
+        txt_cuit.Text = ""
+        txt_documento.Text = ""
+        txt_empresa.Text = ""
+        txt_fijo.Text = ""
+        txt_mail.Text = ""
+        txt_nombre.Text = ""
+
+
+
+    End Sub
+
 End Class
