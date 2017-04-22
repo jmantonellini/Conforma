a437771d8f147ce203f4d38f2f2161c58dfe3364
Mateo <mateochando@gmail.com>
2017-04-22 17:31:38 -0300

    que mierda

--- Trabajo Integral PAV1/Trabajo Integral PAV1/Gestor clientes.vb
+++ Trabajo Integral PAV1/Trabajo Integral PAV1/Gestor clientes.vb
@@ -10,8 +10,13 @@
         _error
     End Enum
 
+<<<<<<< HEAD
     Dim cadena_conexion = "Provider=SQLNCLI11;Data Source=POWERSTATION-PC\SQLEXPRESS2014;Integrated Security=SSPI;Initial Catalog=Conforma"
    
+=======
+   Dim c As Conexion = New Conexion
+
+>>>>>>> parent of f89051d... grilla empresas
     Dim accion As tipo_grabacion = tipo_grabacion.insertar
 
     Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
@@ -26,9 +31,16 @@
 
 
     Private Sub gestor_clientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+<<<<<<< HEAD
         cargar_grilla()
         cargar_combo(cmb_tipo_documento, leer_tabla("TIPOS_DOCUMENTOS") _
                      , "ID_TIPO_DOCUMENTO" _
+=======
+
+        Me.cargar_grilla()
+        cmb_tipo_documento = c.cargar_combo(cmb_tipo_documento, "TIPOS_DOCUMENTOS" _
+                    , "ID_TIPO_DOCUMENTO" _
+>>>>>>> parent of f89051d... grilla empresas
                      , "NOMBRE")
     End Sub
 
@@ -51,6 +63,7 @@
         Me.txt_nombre.Focus()
     End Sub
 
+<<<<<<< HEAD
     Private Sub cargar_combo(ByRef combo As ComboBox _
                              , tabla As Data.DataTable _
                              , pk As String _
@@ -98,4 +111,20 @@
     Private Sub cmd_salir_Click(sender As Object, e As EventArgs) Handles cmd_salir.Click
         Me.Close()
     End Sub
+=======
+    
+    Private Sub cargar_grilla()
+        Me.tabla_clientes.Rows.Clear()
+        Me.tabla_clientes.Columns.Clear()
+        Dim tabla = c.cargar_grilla("clientes")
+        tabla_clientes.DataSource = tabla
+
+    End Sub
+
+    Private Sub cmd_salir_Click(sender As Object, e As EventArgs) Handles cmd_salir.Click
+        Me.Close()
+    End Sub
+
+
+>>>>>>> parent of f89051d... grilla empresas
 End Class
--- Trabajo Integral PAV1/Trabajo Integral PAV1/Gestor empresas.vb
+++ Trabajo Integral PAV1/Trabajo Integral PAV1/Gestor empresas.vb
@@ -1,5 +1,11 @@
 ﻿Public Class gestor_empresas
 
+<<<<<<< HEAD
+=======
+
+    Dim c As Conexion = New Conexion
+
+>>>>>>> parent of f89051d... grilla empresas
     Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         lbl_hora.Text = DateTime.Now.ToString("dd/mm/yyyy HH:mm:ss ")
     End Sub
@@ -8,4 +14,14 @@
         Me.Close()
 
     End Sub
+<<<<<<< HEAD
+=======
+
+   
+    Private Sub gestor_empresas_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim tabla As Data.DataTable = New DataTable
+        tablaEmpresas.DataSource = c.cargar_grilla("empresas")
+
+    End Sub
+>>>>>>> parent of f89051d... grilla empresas
 End Class
