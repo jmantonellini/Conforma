6dbe7c2e57cc0695f9896020b28968d3ada05cdf
gastongarcia1996 <garciagaston1996@gmail.com>
2017-04-22 17:05:14 -0300

    gg

--- Trabajo Integral PAV1/Trabajo Integral PAV1/Gestor empresas.vb
+++ Trabajo Integral PAV1/Trabajo Integral PAV1/Gestor empresas.vb
@@ -4,25 +4,6 @@
         lbl_hora.Text = DateTime.Now.ToString("dd/mm/yyyy HH:mm:ss ")
     End Sub
 
-    Private Sub Label7_Click(sender As Object, e As EventArgs) Handles Label7.Click
-
-    End Sub
-    Private Sub lbl_datos_empresa_Click(sender As Object, e As EventArgs)
-
-    End Sub
-    Private Sub lbl_razon_social_Click(sender As Object, e As EventArgs) Handles lbl_razon_social.Click
-
-    End Sub
-    Private Sub lbl_cuit_Click(sender As Object, e As EventArgs) Handles lbl_cuit.Click
-
-    End Sub
-    Private Sub lbl_nombre_Click(sender As Object, e As EventArgs) Handles lbl_nombre.Click
-
-    End Sub
-    Private Sub lbl_telefono_fijo_Click(sender As Object, e As EventArgs) Handles lbl_telefono_fijo.Click
-
-    End Sub
-
     Private Sub cmd_salir_Click(sender As Object, e As EventArgs) Handles cmd_salir.Click
         Me.Close()
 
