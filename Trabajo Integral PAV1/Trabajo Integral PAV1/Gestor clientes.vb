21fbcddebc64a9010d60d2ccf5b18011ec780413
gastongarcia1996 <garciagaston1996@gmail.com>
2017-04-23 11:35:19 -0300

    proyecto revertido al punto anterior

--- Trabajo Integral PAV1/Trabajo Integral PAV1/Gestor clientes.vb
+++ Trabajo Integral PAV1/Trabajo Integral PAV1/Gestor clientes.vb
@@ -10,63 +10,90 @@
         _error
     End Enum
 
-    Dim cadena_conexion = "Provider=SQLNCLI11;Data Source=JUANMA-PC\SQLEXPRESS2014;Integrated Security=SSPI;Initial Catalog=Conforma"
-   
-
+    Dim cliente As Data.DataTable = New DataTable
+    Dim cadena_conexion = "Provider=SQLNCLI11;Data Source=POWERSTATION-PC\SQLEXPRESS2014;Integrated Security=SSPI;Initial Catalog=Conforma"
+    Dim domicilios As DataTable = New DataTable
+    Dim c As Conexion = New Conexion
+    Dim accion As tipo_grabacion = tipo_grabacion.insertar
 
     Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         lbl_hora.Text = DateTime.Now.ToString("dd/mm/yyyy HH:mm:ss ")
     End Sub
 
+    Private Sub gestor_clientes_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        If MessageBox.Show("¿Seguro que desea salir? Los datos que no hayan sido guardados se perderan", "Advertencia", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) = Windows.Forms.DialogResult.No Then
+            e.Cancel = True
+        End If
+    End Sub
 
-    Private Sub tab_datos_personales_Click(sender As Object, e As EventArgs) Handles tab_datos_personales.Click
 
+    Private Sub gestor_clientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.cargar_grilla()
+        cmb_tipo_documento = c.cargar_combo(cmb_tipo_documento, "TIPOS_DOCUMENTOS" _
+                    , "ID_TIPO_DOCUMENTO" _
+                     , "NOMBRE")
+        tabla_clientes.Rows(0).Selected = True
+        'Me.cargar_cliente()
+        domicilios = c.buscar_domicilios_cliente(tabla_clientes.Item(0, 1).Value, tabla_clientes.Item(0, 0).Value)
+    End Sub
+
+    Private Sub cmd_nuevo_Click(sender As Object, e As EventArgs) Handles cmd_nuevo.Click
+        For Each obj As Windows.Forms.Control In Me.tabla_clientes.Controls
+            If obj.GetType().Name = "TextBox" Then
+                obj.Text = ""
+                obj.Enabled = True
+            End If
+            If obj.GetType().Name = "ComboBox" Then
+                Dim local As ComboBox = obj
+                local.SelectedIndex = -1
+                obj.Enabled = True
+            End If
+            If obj.GetType().Name = "MaskedTextBox" Then
+                obj.Text = ""
+                obj.Enabled = True
+            End If
+        Next
+        Me.accion = tipo_grabacion.insertar
+        Me.cmd_modificar.Enabled = False
+        Me.cmd_guardar.Enabled = True
+        Me.txt_nombre.Focus()
     End Sub
 
     Private Sub cmd_salir_Click(sender As Object, e As EventArgs) Handles cmd_salir.Click
         Me.Close()
     End Sub
 
-    Private Sub cmd_nuevo_Click(sender As Object, e As EventArgs) Handles cmd_nuevo.Click
-        For Each obj As Windows.Forms.Control In Me.Controls
-            If obj.GetType().Name = "TextBox" Then
-                obj.Text = ""
-            End If
-            If obj.GetType().Name = "ComboBox" Then
-                Dim local As ComboBox = obj
-                local.SelectedIndex = -1
-            End If
-            If obj.GetType().Name = "MaskedTextBox" Then
-                obj.Text = ""
-            End If
-        Next
-    End Sub
 
-    Private Sub cargar_clientes()
-        Dim tabla As New DataTable
-
-        Dim index As Integer
+    Private Sub cargar_grilla()
         Me.tabla_clientes.Rows.Clear()
-
-        For index = 0 To tabla.Rows.Count - 1
-            Me.tabla_clientes.Rows.Add()
-            Me.tabla_clientes.Rows(index).Cells(0).Value = tabla.Rows(index)("APELLIDO")
-            Me.tabla_clientes.Rows(index).Cells(1).Value = tabla.Rows(index)("NOMBRE")
-            Me.tabla_clientes.Rows(index).Cells(2).Value = tabla.Rows(index)("CUIL_EMPRESA")
-            Me.tabla_clientes.Rows(index).Cells(3).Value = tabla.Rows(index)("TELEFONO_CELULAR")
-        Next
-
-
-
+        Me.tabla_clientes.Columns.Clear()
+        Dim tabla = c.cargar_grilla("clientes")
+        tabla_clientes.DataSource = tabla
 
     End Sub
 
-    Private Function leer_tabla(ByVal nombre_tabla As String) As Data.DataTable
-        Return Conexion.ejecuto_sql("SELECT * " + nombre_tabla)
-    End Function
+    Private Sub tabla_clientes_Click(sender As Object, e As DataGridViewCellEventArgs) Handles tabla_clientes.CellClick
+        cargar_cliente()
+        cmd_modificar.Enabled = True
+        cmd_eliminar.Enabled = True
 
-    Private Sub gestor_clientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        cargar_clientes()
+    End Sub
+
+    Private Sub cargar_cliente()
+        
+        cliente = c.buscar_datos_cliente(tabla_clientes.CurrentRow.Cells(1).Value, tabla_clientes.CurrentRow.Cells(0).Value)
+        domicilios = c.buscar_domicilios_cliente(tabla_clientes.CurrentRow.Cells(1).Value, tabla_clientes.CurrentRow.Cells(0).Value)
+
+
+        txt_nombre.Text = cliente.Rows(0).Item(3).ToString
+        txt_apellido.Text = cliente.Rows(0).Item(4).ToString
+        txt_documento.Text = cliente.Rows(0).Item(2).ToString
+        txt_cuit.Text = cliente.Rows(0).Item(8).ToString
+        txt_celular.Text = cliente.Rows(0).Item(6).ToString
+        txt_fijo.Text = cliente.Rows(0).Item(7).ToString
+
+
 
     End Sub
 End Class
