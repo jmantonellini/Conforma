1dd81bb596f11d7909fd2c7211b6944027f25207
juaanma12 <juanmantonellini@gmail.com>
2017-04-26 01:27:22 -0300

    asdasd

--- Trabajo Integral PAV1/Trabajo Integral PAV1/Gestor Provincias.vb
+++ Trabajo Integral PAV1/Trabajo Integral PAV1/Gestor Provincias.vb
@@ -2,9 +2,9 @@
 
     Dim c As Conexion = New Conexion
 
-    Private Sub Gestor_Provincias_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        c.cargar_combo(cmb_pais, "PAISES", "ID_PAIS", "NOMBRE")
-        tabla_provincias.DataSource = c.cargar_provincias_filtrada(cmb_pais.Text)
+    'Private Sub Gestor_Provincias_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    '    c.cargar_combo(cmb_pais, "PAISES", "ID_PAIS", "NOMBRE")
+    '    tabla_provincias.DataSource = c.cargar_provincias_filtrada(cmb_pais.Text)
 
-    End Sub
+    'End Sub
 End Class
